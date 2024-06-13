6cbbb10bd688f9b07adae22ec0816eb90ef5ff65
RosDeeper <hoanggg237@gmail.com>
2024-06-13 21:19:11 +0700

    cart-content change async function

--- app/(routes)/cart/components/cart-content.tsx
+++ app/(routes)/cart/components/cart-content.tsx
@@ -35,15 +35,21 @@ const CartContent = ({userId} : CartContentProps) =>{
         }
     },[searchParams, cart.removeAll]);
 
-    const onCheckOut = async () =>{
-        const response  = await axios.post(
-            `${process.env.NEXT_PUBLIC_APP_URL}/checkout`,{
-                product : cart.items,
-                userId
-            }
-        )
+    const onCheckOut = async () => {
+        try {
+            const response = await axios.post(
+                `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+                {
+                    product : cart.items,
+                    userId
+                },
+            )
 
-        window.location = response.data.url 
+            window.location = response.data.url 
+
+        } catch(err) {
+            console.log('Checkout failed: ', err)
+        }
     };
 
     return(
